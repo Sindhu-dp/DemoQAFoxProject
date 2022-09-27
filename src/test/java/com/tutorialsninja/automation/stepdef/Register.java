86243d45ed64833fbe12e5d534ebe11f3f4416bb
Sindhu-dp <sindhuprakash33@gmail.com>
2022-09-27 11:29:50 +0530

    completed automating all the scenarios in the register feature file

---
+++ src/test/java/com/tutorialsninja/automation/stepdef/Register.java
@@ -0,0 +1,96 @@
+package com.tutorialsninja.automation.stepdef;
+
+import java.util.Map;
+
+import org.junit.Assert;
+
+import com.tutorialsninja.automation.base.Base;
+import com.tutorialsninja.automation.framework.Elements;
+import com.tutorialsninja.automation.pages.AccountSuccessPage;
+import com.tutorialsninja.automation.pages.HeadersSection;
+import com.tutorialsninja.automation.pages.RegisterPage;
+
+import cucumber.api.DataTable;
+import cucumber.api.java.en.*;
+
+
+public class Register {
+	HeadersSection headerSection  = new HeadersSection ();
+	RegisterPage registerPage  = new RegisterPage  ();
+	AccountSuccessPage accountSuccessPage = new AccountSuccessPage  ();
+	
+	@Given("^I launch the application$")
+	public void i_launch_the_application() {
+		Base.driver.get(Base.reader.getUrl()); 
+
+	}
+
+	@And("^I navigate to Account Registration page$")
+	public void i_navigate_to_Account_Registration_page()  {
+ Elements.click(HeadersSection.myAccountLink);
+ Elements.click(HeadersSection.register);
+	}
+
+	@And("^I provide all the below valid details$")
+	public void i_provide_all_the_below_valid_details(DataTable dataTable) {
+		RegisterPage.enteralldetails(dataTable );
+	}
+
+	@And("^I select the Privacy Policy$")
+	public void i_select_the_Privacy_Policy()  {
+		Elements.click(RegisterPage.PrivacyPolicy);
+
+	}
+
+	@When("^I click on Continue button$")
+	public void i_click_on_Continue_button() throws Throwable {
+		Elements.click(RegisterPage.ContinueButton);
+	}
+
+	@Then("^I should see that the User Account has successfully created$")
+	public void i_should_see_that_the_User_Account_has_successfully_created() {
+		Assert.assertTrue(Elements.isDisplayed(AccountSuccessPage.successBreadcrum));
+
+	}
+
+	
+	@Then("^I should see that the User Account is not created$")
+	public void i_should_see_that_the_User_Account_is_not_created()  {
+		Assert.assertTrue(Elements.isDisplayed(RegisterPage.Registerpagebreadcrum));   
+	}
+
+	@Then("^I should see the error messages informing the user to fill the mandatory fields$")
+	public void i_should_see_the_error_messages_informing_the_user_to_fill_the_mandatory_fields() {
+		
+		Assert.assertTrue(Elements.isDisplayed(RegisterPage.firstNameWarning));
+		Assert.assertTrue(Elements.isDisplayed(RegisterPage.lastNameWarning));
+		Assert.assertTrue(Elements.isDisplayed(RegisterPage.emailWarning));
+		Assert.assertTrue(Elements.isDisplayed(RegisterPage.telephoneWarning));
+		Assert.assertTrue(Elements.isDisplayed(RegisterPage.passwordWarning));
+		Assert.assertTrue(Elements.isDisplayed(RegisterPage.mainWarning));
+		
+	}
+
+
+@And("^I subscribe to Newsletter$")
+public void i_subscribe_to_Newsletter() throws Throwable {
+ Elements.click(RegisterPage.yesToNewsLetter);
+}
+
+
+@When("^I provide the below duplicate details into the fields$")
+public void i_provide_the_below_duplicate_details_into_the_fields(DataTable dataTable){
+    RegisterPage.enteralldetails(dataTable,"duplicate");
+}
+
+@Then("^I should see the warning message stating that the user is already created$")
+public void i_should_see_the_warning_message_stating_that_the_user_is_already_created() {
+  
+	Assert.assertTrue(Elements.VerifyTextEquals(RegisterPage.mainWarning,"Warning: E-Mail Address is already registered!"));
+	
+}
+
+
+
+	
+}
